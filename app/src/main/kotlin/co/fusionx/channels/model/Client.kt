11409bef9253f08074c42dee8a7197858640a21c
Lalit Maganti <lalitmaganti@gmail.com>
2016-02-05 01:08:22 +0000

    client: temporary fix to join command

--- app/src/main/kotlin/co/fusionx/channels/model/Client.kt
+++ app/src/main/kotlin/co/fusionx/channels/model/Client.kt
@@ -104,9 +104,13 @@ public class Client(
             client.send(ClientGenerator.pong(server))
         }
 
+        // TODO(tilal6991) fix this to work correctly.
         override fun onSocketConnect() {
             client.send(ClientGenerator.nick("tilal6993"))
             client.send(ClientGenerator.user("tilal6993", "Lalit"))
+        }
+
+        override fun onWelcome(target: String, text: String) {
             client.send(ClientGenerator.join("#channels"))
         }
     }
