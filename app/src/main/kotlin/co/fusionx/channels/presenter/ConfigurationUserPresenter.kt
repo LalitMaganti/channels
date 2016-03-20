3f6ea4c74de9f4dc42b7a9b71cd3fe2e18e54557
Lalit Maganti <lalitmaganti@gmail.com>
2016-03-20 19:55:50 +0000

    configuration: connect more UI and backend

--- app/src/main/kotlin/co/fusionx/channels/presenter/ConfigurationUserPresenter.kt
+++ app/src/main/kotlin/co/fusionx/channels/presenter/ConfigurationUserPresenter.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import co.fusionx.channels.activity.ConfigurationEditActivity
 import co.fusionx.channels.activity.helper.EmptyWatcher
 import co.fusionx.channels.databinding.ConfigurationEditUserBinding
+import org.parceler.Parcel
 
 class ConfigurationUserPresenter(override val activity: Activity,
                                  override val binding: ConfigurationEditUserBinding) : ConfigurationEditActivity.Presenter {
@@ -17,4 +18,9 @@ class ConfigurationUserPresenter(override val activity: Activity,
 
         binding.realName.addTextChangedListener(EmptyWatcher(binding.realNameContainer))
     }
+
+    @Parcel(Parcel.Serialization.BEAN)
+    class Configuration(val nicks: List<String> = listOf("ChannelsUser"),
+                        val autoChangeNick: Boolean = true,
+                        val realName: String = "ChannelsUser")
 }
